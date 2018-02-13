1bf0a7cfc8316f353cc2dba874d2f18a14e0bbf7
Victoria Vitale <m.v.vitale@gmail.com>
2018-02-13 02:00:59 -0300

    Adding selection of notes on click

--- src/components/StickyNote.jsx
+++ src/components/StickyNote.jsx
@@ -6,19 +6,25 @@ class StickyNote extends Component {
     super(props);
 
     this.state = {
-      stickyNote : this.props.stickyNote
+      stickyNote : this.props.stickyNote,
+      selected: false
     }
   }
 
   render() {
     return (
       <div>
-          <div className="sticky-note">
+          <div className={"sticky-note" + (this.state.selected ? ' selected' : '')} onClick={this.toggleSelect.bind(this)}>
             <h3>{this.state.stickyNote.title}</h3>
             <textarea placeholder="Double click on a note to edit it">{this.props.stickyNote.content}</textarea>
           </div>
       </div>
     );
+  }
+
+  toggleSelect(){
+    var inverseSelected = !this.state.selected
+    this.setState({ selected: inverseSelected})
   }
 }
 
