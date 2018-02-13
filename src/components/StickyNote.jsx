18698a69fcaa34e1d8cc29d2f573a6d62014ebab
Victoria Vitale <m.v.vitale@gmail.com>
2018-02-13 00:53:22 -0300

    Rendering notes dinamically

--- src/components/StickyNote.jsx
+++ src/components/StickyNote.jsx
@@ -1,11 +1,21 @@
 import React, {Component} from 'react'; 
 
 class StickyNote extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      stickyNote : this.props.stickyNote
+    }
+  }
+
   render() {
     return (
       <div>
           <div className="sticky-note">
-            <textarea>My note content</textarea>
+            <h3>{this.state.stickyNote.title}</h3>
+            <textarea placeholder="Double click on a note to edit it">{this.props.stickyNote.content}</textarea>
           </div>
       </div>
     );
--- src/components/StickyNotesBoard.jsx
+++ src/components/StickyNotesBoard.jsx
@@ -2,15 +2,33 @@ import React, {Component} from 'react';
 import StickyNote from './StickyNote.jsx';
 
 class StickyNotesBoard extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      stickyNotes: []
+    }
+  }
+
   render() {
+    const stickyNotes = this.state.stickyNotes.map((stickyNote) => <StickyNote stickyNote={stickyNote}/>);
+
     return (
       <div>
-        <div className="sticky-notes-container">
-            <StickyNote/>
+        <div className="sticky-notes-container" onDoubleClick={this.addNewNote.bind(this)}>
+          <p>Double click anywhere on the board to add a new note</p>
+          {stickyNotes}
         </div>
       </div>
     );
   }
+
+  addNewNote(){
+    var arrNotes = this.state.stickyNotes;
+    arrNotes.push({id: 1, title: 'New Note', content: ''})
+    this.setState({ stickyNotes: arrNotes})
+  }
+
 }
 
 export default StickyNotesBoard;
